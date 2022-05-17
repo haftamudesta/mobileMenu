97b66ecde5d8ed7e9546447905e53573a30673a2
haftamudesta <desta451616@hotmail.com>
2022-05-17 23:21:34 +0300

    Modified mobile-menu

--- portfolio.js
+++ portfolio.js
@@ -3,35 +3,27 @@ const contacts = document.querySelector('.contacts');
 const closes = document.querySelector('.close');
 const symbols = document.querySelector('.symbols');
 const time = document.querySelector('.time');
-const links = document.querySelector('.links');
+const main = document.querySelector('.main');
 hambergur.addEventListener('click', () => {
   hambergur.classList.toggle('close');
   closes.style.display = 'block';
   hambergur.style.display = 'none';
-  contacts.style.display = 'flex';
-  contacts.style.marginTop = '-60px';
-  contacts.style.marginLeft = '150px';
-  contacts.style.flexDirection = 'column';
-  contacts.style.textDocaration = 'none';
-  contacts.style.color = 'white';
-  contacts.style.position = 'absolute';
-  contacts.style.fontSize = '12px';
-  symbols.style.display = 'none';
-  time.style.display = 'none';
-  links.style.textDecoration = 'none';
+  contacts.style.display = 'block';
+  main.style.opacity = '0';
+  
 });
 
 closes.addEventListener('click', () => {
   closes.classList.toggle('allign');
   hambergur.style.display = 'block';
   closes.style.display = 'none';
-  contacts.classList.remove('active');
-  contacts.style.marginTop = '-600px';
-});
-
-contacts.forEach().addEventListener('click', () => {
-  contacts.style.marginTop = '-600px';
   contacts.style.display = 'none';
-  closes.style.display = 'none';
+  main.style.opacity = '1';
+  
+});
+contacts.addEventListener('click', () => {
   hambergur.style.display = 'block';
+  closes.style.display = 'none';
+  contacts.style.display = 'none';
+  main.style.opacity = '1';
 });
